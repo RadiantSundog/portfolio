f045f885641fa1e4f5ebfa29ffc4fe46b1ca4924
Alexander Levero <Alexander.Levero@gmail.com>
2024-04-12 21:13:07 +0300

    Add new project.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,9 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.jpg";
-import projImg2 from "../assets/img/project-img2.jpg";
-import projImg3 from "../assets/img/project-img3.jpg";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -14,17 +15,26 @@ export const Projects = () => {
       title: "Seedling",
       description: "Home garden maintenance and plant identification",
       imgUrl: projImg1,
+      link: "https://github.com/RadiantSundog/seedling",
     },
     {
       title: "CarCar",
       description: "Automotive business tracking",
       imgUrl: projImg2,
+      link: "https://github.com/RadiantSundog/carcar",
     },
     {
       title: "Tasker",
       description: "Organize team projects and tasks with ease",
       imgUrl: projImg3,
+      link: "https://github.com/RadiantSundog/tasker",
     },
+    {
+      title: "Jot",
+      description: "React Front-end for a note taking app",
+      imgUrl: projImg4,
+      link: "https://github.com/RadiantSundog/jot",
+    }
   ];
 
   return (
